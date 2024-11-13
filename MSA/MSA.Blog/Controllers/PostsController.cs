b3ad7b56227d7add0c87a9212949e63d1e114546
Muhammad Shahid Khan Afridi <muhammadshahidkhanafridi@gmail.com>
2024-11-13 20:25:45 +0500

    Configured API Gateway using Ocelot

---
+++ MSA/MSA.Blog/Controllers/PostsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MSA.Blog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        // GET: api/<PostsController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<PostsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return $"value {id}";
+        }
+
+        // POST api/<PostsController>
+        [HttpPost]
+        public string Post([FromBody] string value)
+        {
+            return value;
+        }
+
+        // PUT api/<PostsController>/5
+        [HttpPut("{id}")]
+        public string Put(int id, [FromBody] string value)
+        {
+            return value + " " + id;
+        }
+
+        // DELETE api/<PostsController>/5
+        [HttpDelete("{id}")]
+        public bool Delete(int id)
+        {
+            if (id > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ MSA/MSA.Gateway/Program.cs
@@ -0,0 +1,35 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Cache.CacheManager;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration)
+    .AddCacheManager(x => 
+    {
+        x.WithDictionaryHandle();
+    });
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+await app.UseOcelot();
+
+app.Run();
